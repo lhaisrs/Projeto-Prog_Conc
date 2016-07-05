3b8206e9d2c8ac3dca44e9054573b36e39a393b8
Lhaís <lhaisrs@gmail.com>
2016-07-05 12:05:25 -0300

    Mudanças no código Arduino

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -7,22 +7,31 @@
 #include <SPI.h>
 #include <String.h>
 #include <Ethernet.h>
+#include <Servo.h>
 
-//byte mac[] = { 60, 6D, C7, 82, D4, 35 }; //Endereço Mac => Obs.: Confirmar o uso deste MAC
-byte ip[] = { 192, 168, 93, 1 }; //Endereço de IP da sua Rede: Retirado do ipconfig -all
+
+byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 }; //Endereço Mac
+byte ip[] = { 192, 168, 0, 101 }; //Endereço de IP da sua Rede: Retirado do ipconfig -all
 EthernetServer server(8090); //Porta do Serviço
 
-int led = 13; //Pino onde o led está conectado
-int buzzer = 10; //Pino onde o buzzer está conectado
+Servo motor; //Criamos a variável do tipo Servo
+int led = 13; //Pino onde o led de alto brilho está conectado
+int buzzer = 12; //Pino onde o buzzer está conectado
+int led_motor = 11; //Pino onde o led auxiliar do sensor motor está conectado
+int senso_motor = 3; //Pino onde o sensor metor está conectado
 String readString = String(30); //String para buscar dados de endereço
 boolean statusLed = false; //Variável para o status do led 
 boolean statusBuzzer = false; //Variável para o status do buzzer
+boolean statusMotor = false; //Variável para o status do motor
 
 void setup(){
-  //Ethernet.begin(mac, ip);  //Inicia o Shield Ethernet => Obs.: Verificar o MAC
+  Ethernet.begin(mac, ip);  //Inicia o Shield Ethernet => Obs.: Verificar o MAC
+  server.begin();
   pinMode(led, OUTPUT); //Definindo o pino ethernet como saída
   pinMode(buzzer, OUTPUT); //Definindo o pino buzzer como saída
+  pinMode(led_motor, OUTPUT); //Definindo o pino do led auxiliar do senso motor
   Serial.begin(9600);  //Inicia a comunicação Serial
+  motor.attach(senso_motor);
 }
 
 void loop(){
@@ -63,14 +72,43 @@ void loop(){
           if(readString.indexOf("LigarBuzzer") >= 0){
             //O Buzzer vai ser ligado
             tone(buzzer, 1500);
-            delay(500);
             statusBuzzer = true;
           }
           if(readString.indexOf("DesligarBuzzer") >= 0){
             //O Buzzer vai ser desligado
             noTone(buzzer);
-            delay(500);
             statusBuzzer = false;
+          }
+
+          //Tratando o servo motor
+          //Usamos          
+          if(readString.indexOf("LigarMotor") >= 0){
+            //O Sensor Motor vai ser ligado
+            int i; 
+            for(i = 0; i < 90; i++){
+              motor.write(i);
+            }
+
+            delay(1000);
+            
+            if(i == 90){
+              digitalWrite(led_motor, HIGH);
+            }
+
+            statusMotor = true;
+          }
+
+          if(readString.indexOf("DesligarMotor") >= 0){
+            //O Sensor Motor vai ser desligado
+            int i; 
+            for(i = 90; i > 0; i--){
+              motor.write(i);
+            }
+            if(i == 0){
+              digitalWrite(led_motor, LOW);
+            }
+
+            statusMotor = false;
           }
 
         //Dados HTML de saída começando com cabeçalho padrão
@@ -87,12 +125,21 @@ void loop(){
         } else {
           client.print("LedDesligado");
         }
+        
         client.print("<font size='20'>");
         client.print("Status Buzzer: ");
         if (statusBuzzer) {
           client.print("BuzzerLigado");
         } else {
           client.print("BuzzerDesligado");
+        }
+
+        client.print("<font size='20'>");
+        client.print("Status Senso Motor: ");
+        if (statusMotor) {
+          client.print("SensoMotorLigado");
+        } else {
+          client.print("SensorMotorDesligado");
         }
         
         //Limpa string para a próxima leitura
